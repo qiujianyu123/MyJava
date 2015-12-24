2562b41430c17a0545da1905db366a090629f62a
qiujianyulyd <879970175@qq.com>
2015-12-24 12:58:46 +0800

    commit.

---
+++ src/com/myjava/cn/AndroidService.java
@@ -0,0 +1,19 @@
+package com.myjava.cn;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class AndroidService {
+
+
+	public static void main(String[] args) throws IOException {
+		ServerSocket serivce = new ServerSocket(8888);
+		while (true) {
+			//等待客户端连接
+			Socket socket = serivce.accept();
+			new Thread(new AndroidRunable(socket)).start();
+		}
+	}
+
+}
