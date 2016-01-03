98c099c8646d0da84629e9e9924af685fbc8dda8
qiujianyulyd <879970175@qq.com>
2016-01-03 22:13:45 +0800

    update -1

--- src/com/myjava/cn/Main.java
+++ src/com/myjava/cn/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("我的年龄：" + Config.AGE);
 		System.out.println("Comment:hello world!");
+		System.out.println("我的世界！");
 	}
 
 }
